43524e65d9b6adaad7e1f9209151bb2910c6fd6b
monalloyd <monapluslloyd@gmail.com>
2023-11-21 11:38:26 +0100

    Write the event controller/service classes

---
+++ backend/src/main/java/com/monalloyd/backend/controller/EventController.java
@@ -0,0 +1,76 @@
+package com.monalloyd.backend.controller;
+
+import com.monalloyd.backend.model.EventType;
+import com.monalloyd.backend.service.event.EventDTO;
+import com.monalloyd.backend.service.event.EventService;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Page;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("events")
+public class EventController {
+    private final EventService eventService;
+
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @GetMapping
+    Page<EventDTO> getAll(Pageable pageable) {
+        return eventService.findAll(pageable);
+    }
+
+    @GetMapping("/{id}")
+    EventDTO getById(@PathVariable long id) {
+        return eventService.findOneById(id)
+                .orElseThrow();
+    }
+
+    @GetMapping("/filter")
+    Page<EventDTO> findByOptionalFilters(
+            @RequestParam(value = "eventType", required = false) EventType eventType,
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+            @RequestParam(value = "start", required = false) LocalDateTime start,
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+            @RequestParam(value = "end", required = false) LocalDateTime end,
+            @RequestParam(value = "street", required = false) String street,
+            @RequestParam(value = "zipcode", required = false) String zipcode,
+            @RequestParam(value = "state", required = false) String state,
+            @RequestParam(value = "city", required = false) String city,
+            @RequestParam(value = "country", required = false) String country,
+            Pageable pageable) {
+        return eventService.findByOptionalFilters(eventType, start, end, street, zipcode, state,
+                city, country, pageable);
+    }
+
+    @GetMapping("/created/{id}")
+    List<EventDTO> findByUserUsername(@PathVariable long id, Authentication authentication) {
+        return eventService.findByUserId(id, authentication);
+    }
+
+    @GetMapping("/types")
+    List<String> getAllEventTypes() {
+        return eventService.findAllEventTypes();
+    }
+
+    @PostMapping
+    EventDTO create(@RequestBody EventDTO eventDTO) {
+        return eventService.save(eventDTO);
+    }
+
+    @PutMapping
+    EventDTO update(@RequestBody EventDTO eventDTO, Authentication authentication) {
+        return eventService.update(eventDTO, authentication);
+    }
+
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable long id, Authentication authentication) {
+        eventService.delete(id, authentication);
+    }
+}
---
+++ backend/src/main/java/com/monalloyd/backend/service/event/EventService.java
@@ -0,0 +1,132 @@
+package com.monalloyd.backend.service.event;
+
+import com.monalloyd.backend.model.*;
+import com.monalloyd.backend.repository.EventRepository;
+import com.monalloyd.backend.repository.LocationRepository;
+import com.monalloyd.backend.repository.UserRepository;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class EventService {
+    private final EventRepository eventRepository;
+    private final LocationRepository locationRepository;
+    private final UserRepository userRepository;
+    private final EventDTOMapper eventDTOMapper;
+    private final EventAuthenticator eventAuthenticator;
+
+    public EventService(EventRepository eventRepository, LocationRepository locationRepository, UserRepository userRepository, EventDTOMapper eventDTOMapper, EventAuthenticator eventAuthenticator) {
+        this.eventRepository = eventRepository;
+        this.locationRepository = locationRepository;
+        this.userRepository = userRepository;
+        this.eventDTOMapper = eventDTOMapper;
+        this.eventAuthenticator = eventAuthenticator;
+    }
+
+    public Page<EventDTO> findAll(Pageable pageable) {
+        Page<Event> events = eventRepository.findAll(pageable, Sort.by("time"));
+        return events.map(eventDTOMapper::eventToDTO);
+    }
+
+    public Optional<EventDTO> findOneById(Long id) {
+        Optional<Event> oEvent = eventRepository.findById(id);
+        if (oEvent.isPresent()) {
+            Event event = oEvent.get();
+            EventDTO dto = eventDTOMapper.eventToDTO(event);
+            return Optional.of(dto);
+        }
+        return Optional.empty();
+    }
+
+    public Page<EventDTO> findByOptionalFilters(
+            EventType eventType, LocalDateTime start, LocalDateTime end,
+            String street, String zipcode, String state, String city, String country, Pageable pageable) {
+        Page<Event> events = eventRepository.findByOptionalFilters(
+                eventType, start, end, street, zipcode, state, city, country, pageable, Sort.by("time"));
+        return events.map(eventDTOMapper::eventToDTO);
+    }
+
+    public List<String> findAllEventTypes() {
+        return Arrays.stream(EventType.values())
+                .map(Enum::name)
+                .collect(Collectors.toList());
+    }
+
+    public EventDTO save(EventDTO eventDTO) {
+        Event event = getEvent(eventDTO);
+        setUser(eventDTO, event);
+        Event createdEvent = eventRepository.save(event);
+        return eventDTOMapper.eventToDTO(createdEvent);
+    }
+
+    private void setUser(EventDTO eventDTO, Event event) {
+        User user = userRepository.findById(Long.parseLong(eventDTO.getUserId())).get();
+        event.setUser(user);
+    }
+
+    private Event getEvent(EventDTO eventDTO) {
+        try {
+            Location savedLocation = locationRepository.save(eventDTO.getLocation());
+            eventDTO.setLocation(savedLocation);
+        } catch (ConstraintViolationException constraintViolationException) {
+            Optional<Location> existingLocation = checkIfAlreadyExists(eventDTO.getLocation());
+            eventDTO.setLocation(existingLocation.get());
+        }
+        return eventDTOMapper.dtoToEvent(eventDTO);
+    }
+
+    private Optional<Location> checkIfAlreadyExists(Location location) {
+        return locationRepository
+                .findByStreetAndZipcodeAndStateAndCityAndCountry(
+                        location.getStreet(),
+                        location.getZipcode(),
+                        location.getState(),
+                        location.getCity(),
+                        location.getCountry()
+                );
+    }
+
+    public EventDTO update(EventDTO eventDTO, Authentication authentication) {
+        Event event = getEvent(eventDTO);
+        setUser(eventDTO, event);
+        if (eventAuthenticator.isOwner(event, authentication)) {
+            Event createdEvent = eventRepository.save(event);
+            return eventDTOMapper.eventToDTO(createdEvent);
+        } else {
+            throw new AccessDeniedException("You are not authorized to perform this action.");
+        }
+    }
+
+    public void delete(long id, Authentication authentication) {
+        Event event = eventRepository.getReferenceById(id);
+        if (eventAuthenticator.isOwner(event, authentication) || eventAuthenticator.isAdmin(authentication)) {
+            eventRepository.deleteById(id);
+        } else {
+            throw new AccessDeniedException("You are not authorized to perform this action.");
+        }
+    }
+
+    public List<EventDTO> findByUserId(long id, Authentication authentication) {
+        User user = userRepository.getReferenceById(id);
+        if (eventAuthenticator.isOwner(user, authentication) || eventAuthenticator.isAdmin(authentication)) {
+            return eventRepository.findByUserId(id, Sort.by("time"))
+                    .stream()
+                    .map(eventDTOMapper::eventToDTO)
+                    .toList();
+        } else {
+            throw new AccessDeniedException("You are not authorized to perform this action.");
+        }
+    }
+}
